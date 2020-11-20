dcfacf73a393569d9fdbb241af8acdbceb04bce9
Blake Donnelly <blake.donnelly2@yahoo.com>
2020-11-20 18:20:37 -0500

    Adjust checkbottom to utilize elementsFromPoint

--- src/Components/Tile/Tile.js
+++ src/Components/Tile/Tile.js
@@ -27,76 +27,104 @@ class Tile extends Component {
   }
 
   checkOverlap = (grouping, x, y) => {
+    if (grouping.classList.contains("App")) return;
     this.checkBottom(grouping.style, x, y);
     this.checkLeft(grouping.style, x, y);
     this.checkRight(grouping.style, x, y);
     this.checkTop(grouping.style, x, y);
     this.grouping.current.style.zIndex = 0;
   };
+  joinX(tile) {
+    return true
+  }
+  joinY(tile) {
+    tile.parentElement.appendChild(this.canvas.current)
+    return true
+  }
   checkMatch = (tile, side) => {
-    const sideToCheck ={
+    if(!tile.dataset){
+      return
+    }
+    const sideToCheck = {
       Bottom: "top",
       Left: "right",
       Right: "left",
-      Top: "bottom"
-    }
-    if (tile.dataset[sideToCheck[side]] === this.canvas.current.id){
-      console.log(`${side} match`)
-      return true
+      Top: "bottom",
+    };
+    if (tile.dataset[sideToCheck[side]] === this.canvas.current.id) {
+      console.log(`${side} match`);
+      if (side === "bottom" || side === "top") return this.joinX(tile);
+      return this.joinY(tile);
     }
   };
 
   checkBottom = ({ height, width }, x, y) => {
     height = parseInt(height);
     width = parseInt(width);
-    let bL = document.elementFromPoint(x, y + height + 1);
-    let bl2 = document.elementsFromPoint(x, y + height + 1)
-    if(this.checkMatch(bL, "Bottom")) return;
-    let bR = document.elementFromPoint(x + width, y + height + 1);
-    if(this.checkMatch(bR, "Bottom")) return;
-    this.moveTile(bL, parseInt(bL.parentElement.style.left), y + height + 2);
+    let bL = document.elementsFromPoint(x, y + height + 1);
+    let bLCanvas = bL.find(element => element.className === "canvas-grouping")
+    this.checkMatch(bL[0], "Bottom")
+    let bR = document.elementsFromPoint(x + width, y + height + 1);
+    let bRCanvas = bR.find(element => element.className === "canvas-grouping" )
+    this.checkMatch(bR[0], "Bottom")
+    console.log(bR, bRCanvas, bLCanvas)
+    if (bLCanvas || bRCanvas){
+    this.moveTile(bL, parseInt(bLCanvas.style.left), y + height + 2);
     if (bL.id !== bR.id)
-      this.moveTile(bR, parseInt(bR.parentElement.style.left), y + height + 2);
+      this.moveTile(bR, parseInt(bRCanvas.style.left), y + height + 2);
+    }
   };
 
   checkLeft = ({ height, width }, x, y) => {
     height = +height.split("px")[0];
     width = +width.split("px")[0];
-    let bL = document.elementFromPoint(x - 1, y + height);
-    if(this.checkMatch(bL, "Left")) return;
-    let tL = document.elementFromPoint(x - 1, y);
-    if(this.checkMatch(tL, "Left")) return;
+    let bL = document.elementsFromPoint(x - 1, y + height);
+    let bLCanvas = bL.find(element => element === "canvas")
+    this.checkMatch(bL, "Left")
+    let tL = document.elementsFromPoint(x - 1, y);
+    let tLCanvas = tL.find(element => element === "canvas")
+    this.checkMatch(tL, "Left")
+    if(tLCanvas){
     this.moveTile(bL, x - width - 2, parseInt(bL.parentElement.style.top));
     if (bL.id !== tL.id)
       this.moveTile(tL, x - width - 2, parseInt(tL.parentElement.style.top));
+    }
   };
 
   checkRight = ({ height, width }, x, y) => {
     height = +height.split("px")[0];
     width = +width.split("px")[0];
-    let bR = document.elementFromPoint(x + width + 1, y + height);
-    if(this.checkMatch(bR, "Right")) return;
-    let tR = document.elementFromPoint(x + width + 1, y);
-    if(this.checkMatch(tR, "Right")) return;
+    let bR = document.elementsFromPoint(x + width + 1, y + height);
+    let bRCanvas = bR.find(element => element === "canvas")
+    this.checkMatch(bR, "Right")
+    let tR = document.elementsFromPoint(x + width + 1, y);
+    let tRCanvas = tR.find(element => element === "canvas")
+    this.checkMatch(tR, "Right")
+    if(bRCanvas){
     this.moveTile(bR, x + width + 2, parseInt(bR.parentElement.style.top));
     if (bR.id !== tR.id)
       this.moveTile(tR, x + width + 2, parseInt(tR.parentElement.style.top));
+    }
   };
 
   checkTop = ({ height, width }, x, y) => {
     height = +height.split("px")[0];
     width = +width.split("px")[0];
-    let tL = document.elementFromPoint(x, y - 1);
-    if(this.checkMatch(tL, "Top")) return;
-    let tR = document.elementFromPoint(x + width, y - 1);
-    if(this.checkMatch(tR, "Top")) return;
+    let tL = document.elementsFromPoint(x, y - 1);
+    let tLCanvas = tL.find(element => element === "canvas")
+    this.checkMatch(tL, "Top")
+    let tR = document.elementsFromPoint(x + width, y - 1);
+    let tRCanvas = tR.find(element => element === "canvas")
+    this.checkMatch(tR, "Top")
+    if (tRCanvas){
     this.moveTile(tL, parseInt(tL.parentElement.style.x), y - height - 2);
     if (tL.id !== tR.id)
       this.moveTile(tR, parseInt(tR.parentElement.style.left), y - height - 2);
+    }
+    
   };
 
   moveTile(tile, newX, newY) {
-    if (tile.classList.contains("App")) return;
     tile.parentElement.style.top = newY + "px";
     tile.parentElement.style.left = newX + "px";
     window.setTimeout(
@@ -112,6 +140,7 @@ class Tile extends Component {
         className="canvas-grouping"
         id={this.props.coordinates.split(",").join("-")}
         ref={this.grouping}
+        data-grouped={false}
         style={{
           height: "100px",
           width: "100px",
