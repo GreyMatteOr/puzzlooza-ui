6e105f616857ae9616562f357e25eb95f6dc701d
Matthew Lane <themplane@gmail.com>
2020-11-19 22:28:47 -0800

    Add Collision detection and recursive pushing.

--- src/Components/Tile/Tile.js
+++ src/Components/Tile/Tile.js
@@ -26,47 +26,61 @@ class Tile extends Component {
     )
   }
 
-  checkOverlap( canvas, x, y ) {
-    this.checkBottom( canvas, x, y );
-    this.checkLeft( canvas, x, y );
-    this.checkRight( canvas, x, y );
-    this.checkTop( canvas, x, y );
+  checkOverlap = ( canvas, x, y ) => {
+    this.checkBottom( canvas.style, x, y );
+    this.checkLeft( canvas.style, x, y );
+    this.checkRight( canvas.style, x, y );
+    this.checkTop( canvas.style, x, y );
+    this.canvas.current.style.zIndex = 0;
   }
 
 
-  checkBottom( {height, width}, x, y ) {
+  checkBottom = ( {height, width}, x, y ) => {
+    height = parseInt(height);
+    width = parseInt(width);
     let bL = document.elementFromPoint(x , y + height + 1);
     let bR = document.elementFromPoint(x + width , y + height + 1)
-    this.moveTile(bL);
-    if (bL.id !== bR.id) this.moveTile(bR);
+    this.moveTile(bL, parseInt(bL.style.left), y + height + 2);
+    if (bL.id !== bR.id) this.moveTile(bR, parseInt(bR.style.left), y + height + 2);
   }
 
-  checkLeft( {height, width}, x, y ) {
+  checkLeft = ( {height, width}, x, y ) => {
+    height = +(height.split('px')[0]);
+    width = +(width.split('px')[0]);
     let bL = document.elementFromPoint(x - 1, y + height);
     let tL = document.elementFromPoint(x - 1 , y);
-    this.moveTile(bL);
-    this.moveTile(tL);
+    this.moveTile(bL, x - width - 2, parseInt(bL.style.top));
+    if (bL.id !== tL.id) this.moveTile(tL, x - width - 2, parseInt(tL.style.top));
   }
 
-  checkRight( {height, width}, x, y ) {
+  checkRight = ( {height, width}, x, y ) => {
+    height = +(height.split('px')[0]);
+    width = +(width.split('px')[0]);
+    console.log(x, width, 1)
     let bR = document.elementFromPoint(x + width + 1 , y + height)
     let tR = document.elementFromPoint(x + width + 1 , y)
-    this.moveTile(bL);
-    this.moveTile(tR);
+    this.moveTile(bR, x + width + 2, parseInt(bR.style.top));
+    console.log()
+    if (bR.id !== tR.id) this.moveTile(tR, x + width + 2, parseInt(tR.style.top));
+
   }
 
-  checkTop( {height, width}, x, y ) {
+  checkTop = ( {height, width}, x, y ) => {
+    height = +(height.split('px')[0]);
+    width = +(width.split('px')[0]);
     let tL = document.elementFromPoint(x, y - 1);
     let tR = document.elementFromPoint(x + width , y - 1)
-    this.moveTile(tL);
-    this.moveTile(tR);
+    this.moveTile(tL, parseInt(tL.style.x), y - height - 2);
+    if (tL.id !== tR.id) this.moveTile(tR, parseInt(tR.style.left), y - height - 2);
   }
 
   moveTile( tile, newX, newY ) {
     if (!tile.classList.contains('App')) {
-      tile.style.top = newY;
-      tile.style.left = newX;
-      window.setTimeout( () => this.checkOverlap(tile), newX, newY, 0);
+      console.log('old', newX, newY)
+      tile.style.top = newY + "px";
+      tile.style.left = newX + "px";
+      console.log(tile)
+      window.setTimeout( () => this.checkOverlap(tile, newX, newY), 0);
     }
   }
 
@@ -77,7 +91,7 @@ class Tile extends Component {
       id={this.props.id}
       style={{
         borderColor: "black",
-        borderStyle: "double",
+        borderStyle: "none",
         height: "100px",
         id: this.props.id,
         width: "100px",
